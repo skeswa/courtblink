43794528e03a6e193eeceea944b757df11069b28
Sandile Keswa <me@sandile.io>
2017-01-24 01:13:35 -0500

    more store things

--- frontend/client/index.js
+++ frontend/client/index.js
@@ -1,9 +1,16 @@
 
 import { h, render } from 'preact'
 
-import style from './index.css'
 import App from 'components/App'
+import style from './index.css'
+import Store from 'store'
 import { fetchSplashData } from 'services'
+
+// Store key constants.
+const STORE_KEY_GAMES = 'games'
+const STORE_KEY_BOX_SCORES = 'boxScores'
+const STORE_KEY_SPLASH_ERROR = 'splashError'
+const STORE_KEY_SPLASH_LOADED = 'splashLoaded'
 
 // Enables React devtools.
 if (process.env.NODE_ENV === 'development') {
@@ -12,8 +19,15 @@ if (process.env.NODE_ENV === 'development') {
 
 // Fetch the splash data.
 fetchSplashData()
-  .then(splashData => console.log('SPLASH DATA', splashData))
-  .catch(err => console.log('fetchSplashData failed:', err))
+  .then(splashData => {
+    Store.update(STORE_KEY_SPLASH_LOADED, true)
+    Store.update(STORE_KEY_GAMES, splashData.games)
+    Store.update(
+      STORE_KEY_BOX_SCORES,
+      splashData.firstGameDetails ? [splashData.firstGameDetails] : [])
+  })
+  .catch(err => Store.update(STORE_KEY_SPLASH_ERROR, err))
 
 const mainEl = document.getElementsByTagName('main')[0]
-render(<App />, mainEl)
+const BoundApp = Store.bind(App, STORE_KEY_SPLASH_LOADED)
+render(<BoundApp />, mainEl)
--- frontend/client/store/index.js
+++ frontend/client/store/index.js
@@ -7,37 +7,41 @@ import Binder from './binder'
 const storeData = new Map()
 const storeEmitter = new EventEmitter()
 
-// update changes a value in the Store.
-export function update(key, value) {
-  storeData.set(key, value)
-  storeEmitter.emit(key, value)
-}
+const Store = {
+  // update changes a value in the Store.
+  update(key, value) {
+    storeData.set(key, value)
+    storeEmitter.emit(key, value)
+  },
 
-// bind binds a component type to the store.
-export function bind() {
-  if (arguments.length < 2) {
-    throw new Error('bind(...) requires at least 2 arguments')
-  }
-  if (!arguments[0]) {
-    throw new Error(
-      'the first argument of bind(...) should be a component type')
-  }
-
-  const boundKeys = []
-  const componentType = arguments[0]
-  for (let i = 1; i < arguments.length; i++) {
-    const boundKey = arguments[i]
-    if (typeof boundKey !== 'string' && !(boundKey instanceof String)) {
-      throw new Error(
-        `argument #${i} of bind(...) was not a valid store key since it was ` +
-        `not a valid string`)
+  // bind binds a component type to the store.
+  bind() {
+    if (arguments.length < 2) {
+      throw new Error('bind(...) requires at least 2 arguments')
     }
-    boundKeys.add(boundKey)
-  }
+    if (!arguments[0]) {
+      throw new Error(
+        'the first argument of bind(...) should be a component type')
+    }
 
-  return new Binder(
-    componentType,
-    boundKeys,
-    storeData,
-    storeEmitter)
+    const boundKeys = []
+    const componentType = arguments[0]
+    for (let i = 1; i < arguments.length; i++) {
+      const boundKey = arguments[i]
+      if (typeof boundKey !== 'string' && !(boundKey instanceof String)) {
+        throw new Error(
+          `argument #${i} of bind(...) was not a valid store key since it ` +
+          `was not a valid string`)
+      }
+      boundKeys.add(boundKey)
+    }
+
+    return new Binder(
+      componentType,
+      boundKeys,
+      storeData,
+      storeEmitter)
+  },
 }
+
+export default Store
