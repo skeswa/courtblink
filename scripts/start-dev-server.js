f9ec7d62b2ea670305100e1ba2ca6262a2150899
Sandile Keswa <me@sandile.io>
2017-10-19 00:38:52 -0400

    added infra and tweaked the dev server script

--- scripts/start-dev-server.js
+++ scripts/start-dev-server.js
@@ -6,43 +6,59 @@ const yarn = require('./common/yarn')
 /** Starts the development server. */
 async function startDevServer() {
   // List of processes to be killed upon exiting.
-  const killables = []
+  const childProcesses = []
 
+  /** Called when the process exits for any reason. */
   function onExit() {
     // Exit early if there are no killable processes.
-    if (killables.length < 1) return
+    if (childProcesses.length < 1) return
 
-    console.log('Killing', killables.length, 'child processes')
+    console.log(`Killing ${childProcesses.length} child processes...`)
 
     // Make sure all of the members of the kill list are killed.
-    killables.forEach(killable => killable.kill('SIGINT'))
+    while (childProcesses.length > 0) {
+      childProcesses.pop().kill('SIGINT')
+    }
+
+    console.log(`Killed ${childProcesses.length} child processes successfully.`)
+  }
+
+  /** Called when the process exits via a signal (like via `ctrl + c`). */
+  function onExitBySignal() {
+    // Handle the exit by executing the cleanup logic.
+    onExit()
+
+    // Make sure that the process dies gracefully.
+    process.exit(0)
+  }
+
+  /** Registers a child process so that we can properly dispose of it later. */
+  function registerChildProcess(childProcess) {
+    childProcesses.push(childProcess)
   }
 
   // Kill the killables when the process exits.
   process.on('exit', onExit)
-  process.on('SIGINT', onExit)
-  process.on('SIGTERM', onExit)
+  process.on('SIGHUP', onExitBySignal)
+  process.on('SIGINT', onExitBySignal)
+  process.on('SIGTERM', onExitBySignal)
 
-  try {
-    // Run the backend and the frontend at the same time.
-    await Promise.all([
-      yarn.start(
-        path.join(__dirname, '..', 'code', 'backend'),
-        'backend',
-        chalk.blue,
-        killables
-      ),
-      yarn.start(
-        path.join(__dirname, '..', 'code', 'frontend'),
-        'frontend',
-        chalk.green,
-        killables
-      ),
-    ])
-  } catch (err) {
-    // Throw the error after all the killables are killed.
-    throw err
-  }
+  // Now that we're ready to handle a process exit, run the backend and the
+  // frontend at the same time.
+  await Promise.all([
+    yarn.start(
+      path.join(__dirname, '..', 'code', 'backend'),
+      'backend',
+      chalk.blue,
+      registerChildProcess
+    ),
+    yarn.start(
+      path.join(__dirname, '..', 'code', 'frontend'),
+      'frontend',
+      chalk.green,
+      registerChildProcess
+    ),
+  ])
 }
 
 startDevServer().catch(err => {
