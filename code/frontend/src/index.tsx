e0e2587add21ca4ae3c4fa599f528b2bbf71bca9
Sandile Keswa <me@sandile.io>
2017-11-01 20:42:14 -0400

    force https in production

--- code/frontend/src/index.tsx
+++ code/frontend/src/index.tsx
@@ -3,6 +3,14 @@ import { h, render } from 'preact'
 import App from 'containers/App'
 import { registerServiceWorker } from 'network/ServiceWorker'
 
+// Before doing anything in production, make sure we're on HTTPS.
+if (process.env.NODE_ENV === 'production') {
+  const isSessionUnencrypted = location.protocol !== 'https:'
+  if (isSessionUnencrypted) {
+    location.href = `https:${location.href.substring(location.protocol.length)}`
+  }
+}
+
 // Enables React devtools.
 if (process.env.NODE_ENV === 'development') {
   require('preact/devtools')
